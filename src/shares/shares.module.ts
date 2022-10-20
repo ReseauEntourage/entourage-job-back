cc185ded88abeb6c373cf82302fbf683abd69946
Emile Bex <emile.bex@gmail.com>
2022-10-20 16:04:15 +0200

    fix(memory): add --optimize_for_size

--- src/shares/shares.module.ts
+++ src/shares/shares.module.ts
@@ -3,12 +3,13 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from 'src/users/users.module';
 import { Share } from './models';
 import { SharesController } from './shares.controller';
+import { SharesControllerOld } from './shares.controller.old';
 import { SharesService } from './shares.service';
 
 @Module({
   imports: [SequelizeModule.forFeature([Share]), UsersModule],
   providers: [SharesService],
-  controllers: [SharesController],
+  controllers: [SharesController, SharesControllerOld],
   exports: [SequelizeModule, SharesService],
 })
 export class SharesModule {}
