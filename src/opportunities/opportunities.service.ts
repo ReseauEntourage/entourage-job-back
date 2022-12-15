2885bae29b4b3c0a39d16c4cbeb946a56c1922c1
PaulEntourage <112417197+PaulEntourage@users.noreply.github.com>
2022-12-15 15:21:25 +0100

    improvements, corrections and test e2e

--- src/opportunities/opportunities.service.ts
+++ src/opportunities/opportunities.service.ts
@@ -298,7 +298,7 @@ export class OpportunitiesService {
     - status
 
     */
-    console.log(query);
+    // console.log(query);
 
     // const options = {
     //   attributes: [...OpportunityCandidateAttributes],
@@ -335,7 +335,8 @@ export class OpportunitiesService {
       include: includeOptions,
       where: whereOptions,
       offset: query.offset,
-      limit: 5,
+      limit: 25,
+      order: [['createdAt', 'DESC']],
       // ...options,
       // where: {
       // [Op.or]: [
