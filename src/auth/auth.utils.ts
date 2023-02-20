2fb8ac47fe7a2ad71c654e9d2d9f30cde1c12061
Emile Bex <emile.bex@gmail.com>
2023-02-20 17:56:10 +0100

    [EN-5737] fix(token): remove note from token

--- src/auth/auth.utils.ts
+++ src/auth/auth.utils.ts
@@ -16,26 +16,52 @@ export function getTokenFromHeaders(
 }
 
 export function getPartialUserForPayload(user: User): PayloadUser {
-  return {
-    id: user.id,
-    email: user.email,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    gender: user.gender,
-    phone: user.phone,
-    address: user.address,
-    zone: user.zone,
-    role: user.role,
-    adminRole: user.adminRole,
-    candidat: user.candidat,
-    coach: user.coach,
-    lastConnection: user.lastConnection,
-    /*
-      createdAt: user.createdAt,
-      updatedAt: user.updatedAt,
-      deletedAt: user.createdAt,
-    */
+  const {
+    id,
+    email,
+    firstName,
+    lastName,
+    gender,
+    phone,
+    address,
+    zone,
+    role,
+    adminRole,
+    lastConnection,
+    coach,
+    candidat,
+  } = user.toJSON();
+
+  const commonAttributes = {
+    id: id,
+    email: email,
+    firstName: firstName,
+    lastName: lastName,
+    gender: gender,
+    phone: phone,
+    address: address,
+    zone: zone,
+    role: role,
+    adminRole: adminRole,
+    lastConnection: lastConnection,
   };
+
+  if (coach) {
+    const { note, ...restCoach } = coach;
+    return {
+      ...commonAttributes,
+      coach: restCoach,
+    };
+  }
+  if (candidat) {
+    const { note, ...restCandidate } = candidat;
+    return {
+      ...commonAttributes,
+      candidat: restCandidate,
+    };
+  }
+
+  return commonAttributes;
 }
 
 const ITERATIONS = 10000;
