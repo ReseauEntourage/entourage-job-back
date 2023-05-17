c20db3a1a3ae963c7e0dffbc8b036eb770acf77e
Emile Bex <emile.bex@gmail.com>
2023-05-17 18:12:46 +0200

    fix(tests): fix tests for first name and last name not capitalized and trimmed

--- tests/contacts/contacts.e2e-spec.ts
+++ tests/contacts/contacts.e2e-spec.ts
@@ -20,7 +20,7 @@ import {
 import { ContactCandidateFormFactory } from './contact-candidate-form.factory';
 import { ContactCompanyFormFactory } from './contact-company-form.factory';
 import { ContactUsFormFactory } from './contact-us-form.factory';
-import { InscriptionCandidateFormFactory } from './inscription-candiate-form.factory';
+import { InscriptionCandidateFormFactory } from './inscription-candidate-form.factory';
 
 describe('Contacts', () => {
   let app: INestApplication;
