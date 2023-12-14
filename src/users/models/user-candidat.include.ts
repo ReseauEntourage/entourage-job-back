92bd62bc3049eefb18be95f569f460f40459dbf1
Emile Bex <emile.bex@gmail.com>
2023-12-14 11:30:42 +0100

    [EN-6638] feat(profile): include profile in user

--- src/users/models/user-candidat.include.ts
+++ src/users/models/user-candidat.include.ts
@@ -1,4 +1,6 @@
 import { Includeable } from 'sequelize';
+import { UserProfile } from 'src/user-profiles/models';
+import { getUserProfileInclude } from 'src/user-profiles/models/user-profile.include';
 import { UserAttributes } from './user.attributes';
 import { User } from './user.model';
 
@@ -8,11 +10,27 @@ export const UserInclude: Includeable[] = [
     as: 'coach',
     attributes: [...UserAttributes],
     paranoid: false,
+    include: [
+      {
+        model: UserProfile,
+        as: 'userProfile',
+        attributes: ['description', 'currentJob'],
+        include: getUserProfileInclude(),
+      },
+    ],
   },
   {
     model: User,
     as: 'candidat',
     attributes: [...UserAttributes],
     paranoid: false,
+    include: [
+      {
+        model: UserProfile,
+        as: 'userProfile',
+        attributes: ['description', 'currentJob'],
+        include: getUserProfileInclude(),
+      },
+    ],
   },
 ];
