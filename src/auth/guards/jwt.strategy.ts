7e4ce0c9c9c54465d17b3970414f2c975d1a8e3b
Emile Bex <emile.bex@gmail.com>
2024-01-03 14:29:47 +0100

    fix(auth): fix user payload

--- src/auth/guards/jwt.strategy.ts
+++ src/auth/guards/jwt.strategy.ts
@@ -22,6 +22,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     if (!user) {
       throw new UnauthorizedException();
     }
-    return user;
+    return user.toJSON();
   }
 }
