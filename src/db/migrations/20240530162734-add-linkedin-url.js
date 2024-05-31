27c6b9f47e992cec57136c538adda60336bffd0c
Dorian <dorian.pizzato@entourage.social>
2024-05-31 15:12:01 +0200

    Fix migration

--- src/db/migrations/20240530162734-add-linkedin-url.js
+++ src/db/migrations/20240530162734-add-linkedin-url.js
@@ -2,14 +2,14 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('User_Profiles', 'linkedinLink', {
+    return queryInterface.addColumn('User_Profiles', 'linkedinUrl', {
       allowNull: true,
       type: Sequelize.STRING,
-      defaultValue: NULL,
+      defaultValue: null,
     });
   },
 
   down: (queryInterface) => {
-    return queryInterface.removeColumn('User_Profiles', 'linkedinLink');
+    return queryInterface.removeColumn('User_Profiles', 'linkedinUrl');
   },
 };
