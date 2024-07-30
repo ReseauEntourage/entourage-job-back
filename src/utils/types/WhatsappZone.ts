8faadc618723b06aec5bbbc9b8f14bf2b21f34d8
Guillaume Cauchois <guillobits@gmail.com>
2024-07-30 15:27:12 +0200

    [EN-7190] Refacto

--- src/utils/types/WhatsappZone.ts
+++ src/utils/types/WhatsappZone.ts
@@ -3,32 +3,34 @@ import { AdminZones } from 'src/utils/types';
 export const WhatsappByZone = {
   [AdminZones.PARIS]: {
     name: 'IDF // Paris, 92, 93',
-    qrcode_url: process.env.WHATSAPP_COACH_QRCODE_PARIS as string,
+    qrCodePath: '/static/img/whatsapp-qrcode-coach/whatsapp-qrcode-paris.png',
     url: process.env.WHATSAPP_COACH_URL_PARIS as string,
   },
   [AdminZones.LYON]: {
     name: 'Rhône // Lyon',
-    qrcode_url: process.env.WHATSAPP_COACH_QRCODE_LYON as string,
+    qrCodePath: '/static/img/whatsapp-qrcode-coach/whatsapp-qrcode-lyon.png',
     url: process.env.WHATSAPP_COACH_URL_LYON as string,
   },
   [AdminZones.LILLE]: {
     name: 'Nord // Lille',
-    qrcode_url: process.env.WHATSAPP_COACH_QRCODE_LILLE as string,
+    qrCodePath: '/static/img/whatsapp-qrcode-coach/whatsapp-qrcode-lille.png',
     url: process.env.WHATSAPP_COACH_URL_LILLE as string,
   },
   [AdminZones.RENNES]: {
     name: 'Bretagne // Rennes & Lorient',
-    qrcode_url: process.env.WHATSAPP_COACH_QRCODE_RENNES_ORIENT as string,
+    qrCodePath:
+      '/static/img/whatsapp-qrcode-coach/whatsapp-qrcode-rennes-lorient.png',
     url: process.env.WHATSAPP_COACH_URL_RENNES_ORIENT as string,
   },
   [AdminZones.LORIENT]: {
     name: 'Bretagne // Rennes & Lorient',
-    qrcode_url: process.env.WHATSAPP_COACH_QRCODE_RENNES_LORIENT as string,
+    qrCodePath:
+      '/static/img/whatsapp-qrcode-coach/whatsapp-qrcode-rennes-lorient.png',
     url: process.env.WHATSAPP_COACH_URL_RENNES_LORIENT as string,
   },
   [AdminZones.HZ]: {
     name: 'Entourage Pro France',
-    qrcode_url: process.env.WHATSAPP_COACH_QRCODE_HZ as string,
+    qrCodePath: '/static/img/whatsapp-qrcode-coach/whatsapp-qrcode-hz.png',
     url: process.env.WHATSAPP_COACH_URL_HZ as string,
   },
 };
