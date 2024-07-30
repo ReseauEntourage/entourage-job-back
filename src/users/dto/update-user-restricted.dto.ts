abd0818119fadc917f52157d0048fbaddc011987
Guillaume Cauchois <guillobits@gmail.com>
2024-07-30 12:48:28 +0200

    [EN-7212] Add - firstName and lastName in update user

--- src/users/dto/update-user-restricted.dto.ts
+++ src/users/dto/update-user-restricted.dto.ts
@@ -2,5 +2,11 @@ import { PartialType, PickType } from '@nestjs/swagger';
 import { UpdateUserDto } from './update-user.dto';
 
 export class UpdateUserRestrictedDto extends PartialType(
-  PickType(UpdateUserDto, ['email', 'phone', 'address'] as const)
+  PickType(UpdateUserDto, [
+    'firstName',
+    'lastName',
+    'email',
+    'phone',
+    'address',
+  ] as const)
 ) {}
