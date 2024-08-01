6880b988d7e443185603fb5a7e0e04b4d71e7033
Guillaume Cauchois <guillobits@gmail.com>
2024-08-01 16:40:44 +0200

    [EN-7399] Refacto - SlackModule

--- src/external-services/slack/slack.service.ts
+++ src/external-services/slack/slack.service.ts
@@ -3,20 +3,21 @@ import { App, Block, KnownBlock } from '@slack/bolt';
 
 @Injectable()
 export class SlackService {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  sendMessage = async (
-    channel: string,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    blocks: (Block | KnownBlock)[],
-    message: string
-  ): Promise<void> => {
-    const app = new App({
+  private app: App;
+
+  constructor() {
+    this.app = new App({
       token: process.env.SLACK_BOT_TOKEN,
       signingSecret: process.env.SLACK_SIGNING_SECRET,
     });
-
+  }
+  sendMessage = async (
+    channel: string,
+    blocks: (Block | KnownBlock)[],
+    message: string
+  ): Promise<void> => {
     try {
-      await app.client.chat.postMessage({
+      await this.app.client.chat.postMessage({
         channel: channel,
         text: message,
         token: process.env.SLACK_BOT_TOKEN,
