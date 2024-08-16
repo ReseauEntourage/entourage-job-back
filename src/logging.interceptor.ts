c3ae5619fe8db45d0fac4d73ecc1369356584387
Guillaume Cauchois <guillobits@gmail.com>
2024-08-16 14:19:31 +0200

    [App] Debug - LoggingInterceptor

--- src/logging.interceptor.ts
+++ src/logging.interceptor.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import {
   Injectable,
   NestInterceptor,
@@ -9,13 +10,16 @@ import tracer from 'dd-trace';
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler) {
+    console.log('LoggingInterceptor');
     const request = context.switchToHttp().getRequest();
     const clientIp =
       request.headers['x-forwarded-for'] || request.connection.remoteAddress;
 
     const span = tracer.scope().active();
 
+    console.log('clientIp', clientIp);
     if (span) {
+      console.log('span', span);
       span.setTag('http.client_ip', clientIp);
     }
 
