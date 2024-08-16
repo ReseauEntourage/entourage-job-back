fe6f1dad59279b17108af20c4328f127cf43b708
Guillaume Cauchois <guillobits@gmail.com>
2024-08-16 15:29:55 +0200

    [App] APM now include IP address inside Span tag DD

--- src/logging.interceptor.ts
+++ src/logging.interceptor.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import {
   Injectable,
   NestInterceptor,
@@ -11,18 +10,14 @@ import { tap } from 'rxjs/operators';
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler) {
-    console.log('LoggingInterceptor');
     const request = context.switchToHttp().getRequest();
     const clientIp =
       request.headers['x-forwarded-for'] || request.connection.remoteAddress;
 
     const span = tracer.scope().active();
 
-    console.log('clientIp', clientIp);
     if (span) {
-      console.log('span', span);
-      const res = span.setTag('http.client_ip', clientIp);
-      console.log('res', res);
+      span.setTag('http.client_ip', clientIp);
     }
 
     return next.handle().pipe(
