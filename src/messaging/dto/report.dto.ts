587bee228e4c6b1bb2a17adbf0f2ae03d763b922
Guillaume Cauchois <guillobits@gmail.com>
2024-09-09 16:16:52 +0200

    [EN-7454] fix(Messaging): ConversationReport

--- src/messaging/dto/report.dto.ts
+++ src/messaging/dto/report.dto.ts
@@ -1,8 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
-export class ReportAbuseDto {
+export class ReportDto {
   @ApiProperty()
   @IsString()
   reason: string;
+
+  @ApiProperty()
+  @IsString()
+  comment: string;
 }
