4d08ce2a47dcf8d0443e18a9dbaf7382ed2f184c
Dorian P. <dorian.pizzato@gmail.com>
2024-10-09 14:45:44 +0200

    Fix/recommendations timeout2 (#214)
    * Logging
    
    * Group two queries into one
    
    * Adding includes
    
    * Returning expected attributes

--- src/user-profiles/user-profiles.service.ts
+++ src/user-profiles/user-profiles.service.ts
@@ -440,10 +440,12 @@ export class UserProfilesService {
     );
   }
 
+  // V3
   async updateRecommendationsByUserId(userId: string) {
-    console.log('updateRecommendationsByUserId', userId);
-    const user = await this.findOneUser(userId);
-    const userProfile = await this.findOneByUserId(userId);
+    const [user, userProfile] = await Promise.all([
+      this.findOneUser(userId),
+      this.findOneByUserId(userId),
+    ]);
 
     const rolesToFind = isRoleIncluded(CandidateUserRoles, user.role)
       ? [UserRoles.COACH]
@@ -464,18 +466,13 @@ export class UserProfilesService {
       ...userProfile.networkBusinessLines,
     ];
 
-    const helpsOptions: WhereOptions<HelpNeed | HelpOffer> =
-      helps?.length > 0
-        ? {
-            name: {
-              [Op.or]: helps.map(({ name }) => name),
-            },
-          }
+    const helpsOptions =
+      helps.length > 0
+        ? { name: { [Op.or]: helps.map(({ name }) => name) } }
         : {};
 
-    console.log('----------------------------');
-
-    const filteredProfiles = await this.userProfileModel.findAll({
+    // Requête combinée pour récupérer les profils filtrés et leurs détails
+    const profiles = await this.userProfileModel.findAll({
       attributes: ['id'],
       where: {
         isAvailable: true,
@@ -498,24 +495,23 @@ export class UserProfilesService {
           model: HelpNeed,
           as: 'helpNeeds',
           required: false,
-          attributes: ['id'],
-          ...(isRoleIncluded(CandidateUserRoles, rolesToFind)
-            ? { where: helpsOptions }
-            : {}),
+          attributes: ['name'],
+          where: isRoleIncluded(CandidateUserRoles, rolesToFind)
+            ? helpsOptions
+            : {},
         },
         {
           model: HelpOffer,
           as: 'helpOffers',
           required: false,
-          attributes: ['id'],
-          ...(isRoleIncluded(CoachUserRoles, rolesToFind)
-            ? { where: helpsOptions }
-            : {}),
+          attributes: ['name'],
+          where: isRoleIncluded(CoachUserRoles, rolesToFind)
+            ? helpsOptions
+            : {},
         },
         {
           model: User,
           as: 'user',
-          attributes: ['id'],
           include: [
             {
               model: InternalMessage,
@@ -532,46 +528,14 @@ export class UserProfilesService {
               where: { addresseeUserId: userId },
             },
           ],
-          where: {
-            role: rolesToFind,
-          },
+
+          attributes: ['id'],
+          where: { role: rolesToFind },
         },
       ],
-      // limit: 3,
-      logging(sql, timing) {
-        console.log('Filtered profiles');
-        console.log(sql, timing);
-      },
-      benchmark: true,
     });
 
-    console.log('filteredProfiles LENGTH', filteredProfiles.length);
-    console.log('filteredProfiles', filteredProfiles);
-
-    console.log('----------------------------');
-
-    const profiles = await this.userProfileModel.findAll({
-      attributes: UserProfilesAttributes,
-      where: {
-        id: { [Op.in]: filteredProfiles.map(({ id }) => id) },
-      },
-      include: [
-        ...getUserProfileInclude(),
-        {
-          model: User,
-          as: 'user',
-          attributes: UserProfilesUserAttributes,
-        },
-      ],
-      logging(sql, timing) {
-        console.log('Profiles');
-        console.log(sql, timing);
-      },
-      benchmark: true,
-    });
-
-    console.log('PROFILES LENGTH', filteredProfiles.length);
-    console.log('PROFILES', filteredProfiles);
+    // Tri des profils
 
     const sortedProfiles = _.orderBy(
       profiles,
@@ -615,6 +579,404 @@ export class UserProfilesService {
       sortedProfiles.slice(0, 3).map((profile) => profile.user.id)
     );
   }
+
+  // V2
+  // async updateRecommendationsByUserId(userId: string) {
+  //   console.log('updateRecommendationsByUserId', userId);
+
+  //   // Récupérer les informations utilisateur et profil en parallèle
+  //   const [user, userProfile] = await Promise.all([
+  //     this.findOneUser(userId),
+  //     this.findOneByUserId(userId),
+  //   ]);
+
+  //   const rolesToFind = isRoleIncluded(CandidateUserRoles, user.role)
+  //     ? [UserRoles.COACH]
+  //     : CandidateUserRoles;
+
+  //   const sameRegionDepartmentsOptions = userProfile.department
+  //     ? Departments.filter(
+  //         ({ region }) =>
+  //           region ===
+  //           Departments.find(({ name }) => userProfile.department === name)
+  //             .region
+  //       ).map(({ name }) => name)
+  //     : Departments.map(({ name }) => name);
+
+  //   const helps = [...userProfile.helpNeeds, ...userProfile.helpOffers];
+  //   const businessLines = [
+  //     ...userProfile.searchBusinessLines,
+  //     ...userProfile.networkBusinessLines,
+  //   ];
+
+  //   const helpsOptions =
+  //     helps.length > 0
+  //       ? { name: { [Op.or]: helps.map(({ name }) => name) } }
+  //       : {};
+
+  //   // Récupération des profils filtrés
+  //   const filteredProfiles = await this.userProfileModel.findAll({
+  //     // ...getUserProfileAmbitionsInclude(),
+  //     // ...getUserProfileBusinessLinesInclude(),
+  //     attributes: ['id'],
+  //     where: {
+  //       isAvailable: true,
+  //       department: sameRegionDepartmentsOptions,
+  //       [Op.not]: {
+  //         ...(isRoleIncluded(CandidateUserRoles, rolesToFind)
+  //           ? { '$helpNeeds.id$': null }
+  //           : {}),
+  //         ...(isRoleIncluded(CoachUserRoles, rolesToFind)
+  //           ? { '$helpOffers.id$': null }
+  //           : {}),
+  //       },
+  //     },
+  //     include: [
+  //       {
+  //         model: HelpNeed,
+  //         as: 'helpNeeds',
+  //         required: false,
+  //         attributes: ['id'],
+  //         where: isRoleIncluded(CandidateUserRoles, rolesToFind)
+  //           ? helpsOptions
+  //           : {},
+  //       },
+  //       {
+  //         model: HelpOffer,
+  //         as: 'helpOffers',
+  //         required: false,
+  //         attributes: ['id'],
+  //         where: isRoleIncluded(CoachUserRoles, rolesToFind)
+  //           ? helpsOptions
+  //           : {},
+  //       },
+  //       {
+  //         model: User,
+  //         as: 'user',
+  //         attributes: ['id'],
+  //         where: { role: rolesToFind },
+  //       },
+  //     ],
+  //     order: [[{ model: User, as: 'user' }, 'lastConnection', 'DESC']],
+  //     limit: 100,
+  //     subQuery: false,
+  //     logging(sql, timing) {
+  //       console.log('Filtered profiles');
+  //       console.log(sql, timing);
+  //     },
+  //     benchmark: true,
+  //   });
+
+  //   console.log('filteredProfiles LENGTH', filteredProfiles.length);
+
+  //   const profiles = await this.userProfileModel.findAll({
+  //     attributes: UserProfilesAttributes,
+  //     where: {
+  //       id: { [Op.in]: filteredProfiles.map(({ id }) => id) },
+  //     },
+  //     include: [
+  //       ...getUserProfileInclude(),
+  //       {
+  //         model: User,
+  //         as: 'user',
+  //         attributes: UserProfilesUserAttributes,
+  //       },
+  //     ],
+  //     logging(sql, timing) {
+  //       console.log('Filtered profiles');
+  //       console.log(sql, timing);
+  //     },
+  //     benchmark: true,
+  //   });
+
+  //   const sortedProfiles = _.orderBy(
+  //     profiles,
+  //     [
+  //       (profile) => {
+  //         const profileBusinessLines = [
+  //           ...profile.searchBusinessLines,
+  //           ...profile.networkBusinessLines,
+  //         ];
+  //         const businessLinesMatching =
+  //           _.intersection(
+  //             businessLines.map(({ name }) => name),
+  //             profileBusinessLines.map(({ name }) => name)
+  //           ).length * UserProfileRecommendationsWeights.BUSINESS_LINES;
+
+  //         const profileHelps = [...profile.helpOffers, ...profile.helpNeeds];
+  //         const helpsMatching =
+  //           _.intersection(
+  //             helps.map(({ name }) => name),
+  //             profileHelps.map(({ name }) => name)
+  //           ).length * UserProfileRecommendationsWeights.HELPS;
+
+  //         return businessLinesMatching + helpsMatching;
+  //       },
+  //       ({ department }) => department === userProfile.department,
+  //       ({ user: { createdAt } }) => createdAt,
+  //     ],
+  //     ['desc', 'asc', 'desc']
+  //   );
+
+  //   return this.createRecommendations(
+  //     userId,
+  //     sortedProfiles.slice(0, 3).map((profile) => profile.user.id)
+  //   );
+  // }
+
+  // Old Version just in case
+  // async updateRecommendationsByUserId(userId: string) {
+  //   console.log('updateRecommendationsByUserId', userId);
+  //   const user = await this.findOneUser(userId);
+  //   const userProfile = await this.findOneByUserId(userId);
+
+  //   const rolesToFind = isRoleIncluded(CandidateUserRoles, user.role)
+  //     ? [UserRoles.COACH]
+  //     : CandidateUserRoles;
+
+  //   const sameRegionDepartmentsOptions = userProfile.department
+  //     ? Departments.filter(
+  //         ({ region }) =>
+  //           region ===
+  //           Departments.find(({ name }) => userProfile.department === name)
+  //             .region
+  //       ).map(({ name }) => name)
+  //     : Departments.map(({ name }) => name);
+
+  //   const helps = [...userProfile.helpNeeds, ...userProfile.helpOffers];
+  //   const businessLines = [
+  //     ...userProfile.searchBusinessLines,
+  //     ...userProfile.networkBusinessLines,
+  //   ];
+
+  //   const helpsOptions: WhereOptions<HelpNeed | HelpOffer> =
+  //     helps?.length > 0
+  //       ? {
+  //           name: {
+  //             [Op.or]: helps.map(({ name }) => name),
+  //           },
+  //         }
+  //       : {};
+
+  //   console.log('----------------------------');
+
+  //   // const filteredProfiles = await this.userProfileModel.findAll({
+  //   //   attributes: ['id'],
+  //   //   where: {
+  //   //     isAvailable: true,
+  //   //     department: sameRegionDepartmentsOptions,
+  //   //     '$user.receivedMessages.id$': null,
+  //   //     '$user.sentMessages.id$': null,
+  //   //     [Op.not]: {
+  //   //       ...(isRoleIncluded(CandidateUserRoles, rolesToFind)
+  //   //         ? { '$helpNeeds.id$': null }
+  //   //         : {}),
+  //   //       ...(isRoleIncluded(CoachUserRoles, rolesToFind)
+  //   //         ? { '$helpOffers.id$': null }
+  //   //         : {}),
+  //   //     },
+  //   //   },
+  //   //   include: [
+  //   //     ...getUserProfileAmbitionsInclude(),
+  //   //     ...getUserProfileBusinessLinesInclude(),
+  //   //     {
+  //   //       model: HelpNeed,
+  //   //       as: 'helpNeeds',
+  //   //       required: false,
+  //   //       attributes: ['id'],
+  //   //       ...(isRoleIncluded(CandidateUserRoles, rolesToFind)
+  //   //         ? { where: helpsOptions }
+  //   //         : {}),
+  //   //     },
+  //   //     {
+  //   //       model: HelpOffer,
+  //   //       as: 'helpOffers',
+  //   //       required: false,
+  //   //       attributes: ['id'],
+  //   //       ...(isRoleIncluded(CoachUserRoles, rolesToFind)
+  //   //         ? { where: helpsOptions }
+  //   //         : {}),
+  //   //     },
+  //   //     {
+  //   //       model: User,
+  //   //       as: 'user',
+  //   //       attributes: ['id'],
+  //   //       include: [
+  //   //         {
+  //   //           model: InternalMessage,
+  //   //           as: 'receivedMessages',
+  //   //           required: false,
+  //   //           attributes: ['id'],
+  //   //           where: { senderUserId: userId },
+  //   //         },
+  //   //         {
+  //   //           model: InternalMessage,
+  //   //           as: 'sentMessages',
+  //   //           required: false,
+  //   //           attributes: ['id'],
+  //   //           where: { addresseeUserId: userId },
+  //   //         },
+  //   //       ],
+  //   //       where: {
+  //   //         role: rolesToFind,
+  //   //       },
+  //   //     },
+  //   //   ],
+  //   //   // limit: 3,
+  //   //   logging(sql, timing) {
+  //   //     console.log('Filtered profiles');
+  //   //     console.log(sql, timing);
+  //   //   },
+  //   //   benchmark: true,
+  //   // });
+  //   const whereConditions = {
+  //     isAvailable: true,
+  //     department: sameRegionDepartmentsOptions,
+  //     '$user.receivedMessages.id$': null as null,
+  //     '$user.sentMessages.id$': null as null, // Explicitly define the type as null
+  //     ...((isRoleIncluded(CandidateUserRoles, rolesToFind) ||
+  //       isRoleIncluded(CoachUserRoles, rolesToFind)) && {
+  //       [Op.not]: {
+  //         ...(isRoleIncluded(CandidateUserRoles, rolesToFind)
+  //           ? { '$helpNeeds.id$': null }
+  //           : {}),
+  //         ...(isRoleIncluded(CoachUserRoles, rolesToFind)
+  //           ? { '$helpOffers.id$': null }
+  //           : {}),
+  //       },
+  //     }),
+  //   };
+
+  //   const includeConditions = [
+  //     ...getUserProfileAmbitionsInclude(),
+  //     ...getUserProfileBusinessLinesInclude(),
+  //     {
+  //       model: HelpNeed,
+  //       as: 'helpNeeds',
+  //       required: false,
+  //       attributes: ['id'],
+  //       where: isRoleIncluded(CandidateUserRoles, rolesToFind)
+  //         ? helpsOptions
+  //         : undefined,
+  //     },
+  //     {
+  //       model: HelpOffer,
+  //       as: 'helpOffers',
+  //       required: false,
+  //       attributes: ['id'],
+  //       where: isRoleIncluded(CoachUserRoles, rolesToFind)
+  //         ? helpsOptions
+  //         : undefined,
+  //     },
+  //     {
+  //       model: User,
+  //       as: 'user',
+  //       attributes: ['id'],
+  //       where: {
+  //         role: rolesToFind,
+  //       },
+  //       include: [
+  //         {
+  //           model: InternalMessage,
+  //           as: 'receivedMessages',
+  //           required: false,
+  //           attributes: ['id'],
+  //           where: { senderUserId: userId },
+  //         },
+  //         {
+  //           model: InternalMessage,
+  //           as: 'sentMessages',
+  //           required: false,
+  //           attributes: ['id'],
+  //           where: { addresseeUserId: userId },
+  //         },
+  //       ],
+  //     },
+  //   ];
+
+  //   const filteredProfiles = await this.userProfileModel.findAll({
+  //     attributes: ['id'],
+  //     where: whereConditions,
+  //     include: includeConditions,
+  //     // limit: 3,
+  //     logging(sql, timing) {
+  //       console.log('Filtered profiles');
+  //       console.log(sql, timing);
+  //     },
+  //     benchmark: true,
+  //     // Ajoutez des indexations ou des options de performance supplémentaires ici si nécessaire
+  //   });
+
+  //   console.log('filteredProfiles LENGTH', filteredProfiles.length);
+  //   console.log('filteredProfiles', filteredProfiles);
+
+  //   console.log('----------------------------');
+
+  //   const profiles = await this.userProfileModel.findAll({
+  //     attributes: UserProfilesAttributes,
+  //     where: {
+  //       id: { [Op.in]: filteredProfiles.map(({ id }) => id) },
+  //     },
+  //     include: [
+  //       ...getUserProfileInclude(),
+  //       {
+  //         model: User,
+  //         as: 'user',
+  //         attributes: UserProfilesUserAttributes,
+  //       },
+  //     ],
+  //     logging(sql, timing) {
+  //       console.log('Profiles');
+  //       console.log(sql, timing);
+  //     },
+  //     benchmark: true,
+  //   });
+
+  //   console.log('PROFILES LENGTH', filteredProfiles.length);
+  //   console.log('PROFILES', filteredProfiles);
+
+  //   const sortedProfiles = _.orderBy(
+  //     profiles,
+  //     [
+  //       (profile) => {
+  //         const profileBusinessLines = [
+  //           ...profile.searchBusinessLines,
+  //           ...profile.networkBusinessLines,
+  //         ];
+
+  //         const businessLinesDifference = _.difference(
+  //           businessLines.map(({ name }) => name),
+  //           profileBusinessLines.map(({ name }) => name)
+  //         );
+
+  //         const businessLinesMatching =
+  //           (businessLines.length - businessLinesDifference.length) *
+  //           UserProfileRecommendationsWeights.BUSINESS_LINES;
+
+  //         const profileHelps = [...profile.helpOffers, ...profile.helpNeeds];
+
+  //         const helpsDifferences = _.difference(
+  //           helps.map(({ name }) => name),
+  //           profileHelps.map(({ name }) => name)
+  //         );
+
+  //         const helpsMatching =
+  //           (helps.length - helpsDifferences.length) *
+  //           UserProfileRecommendationsWeights.HELPS;
+
+  //         return businessLinesMatching + helpsMatching;
+  //       },
+  //       ({ department }) => department === userProfile.department,
+  //       ({ user: { createdAt } }) => createdAt,
+  //     ],
+  //     ['desc', 'asc', 'desc']
+  //   );
+
+  //   return this.createRecommendations(
+  //     userId,
+  //     sortedProfiles.slice(0, 3).map((profile) => profile.user.id)
+  //   );
+  // }
 
   async uploadProfileImage(userId: string, file: Express.Multer.File) {
     const { path } = file;
