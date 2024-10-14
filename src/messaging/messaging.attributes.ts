12c4c9bd884fe7e3251b562c27a82e3350cccee7
Guillaume Cauchois <guillobits@gmail.com>
2024-10-14 17:22:38 +0200

    refacto: messaging includes & attributes

---
+++ src/messaging/messaging.attributes.ts
@@ -0,0 +1,13 @@
+export const userAttributes = [
+  'id',
+  'firstName',
+  'lastName',
+  'email',
+  'gender',
+  'role',
+  'zone',
+  'deletedAt',
+];
+
+export const messageAttributes = ['id', 'content', 'createdAt', 'authorId'];
+export const userProfileAttributes = ['id', 'isAvailable'];
