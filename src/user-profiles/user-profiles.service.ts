4f7f841bebf1ccbd2e0d9fc4d06bc12ef3a972f8
Dorian <dorian.pizzato@entourage.social>
2024-10-15 15:42:04 +0200

    Enabling logging on recommendations

--- src/user-profiles/user-profiles.service.ts
+++ src/user-profiles/user-profiles.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import fs from 'fs';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
@@ -538,6 +539,11 @@ export class UserProfilesService {
           ],
         },
       ],
+      logging: (sql, timing) => {
+        console.log(sql);
+        console.log(timing);
+      },
+      benchmark: true,
     });
 
     const sortedProfiles = _.orderBy(
