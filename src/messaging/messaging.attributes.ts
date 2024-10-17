c67c14b97c6500ca6e65cacf4d5cb16ea2ac15b1
Guillaume Cauchois <guillobits@gmail.com>
2024-10-17 16:27:13 +0200

    fix: remove useless user attributes in messaging

--- src/messaging/messaging.attributes.ts
+++ src/messaging/messaging.attributes.ts
@@ -2,11 +2,9 @@ export const userAttributes = [
   'id',
   'firstName',
   'lastName',
-  'email',
   'gender',
   'role',
   'zone',
-  'deletedAt',
 ];
 
 export const messageAttributes = ['id', 'content', 'createdAt', 'authorId'];
