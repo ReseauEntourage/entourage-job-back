8e31c0b4056a19fdb594d4366ffb2b2c41ccafe0
Guillaume Cauchois <guillobits@gmail.com>
2025-01-09 13:34:12 +0100

    feat(script sync-user-profile-has-picture): refacto

--- scripts/sync-user-profile-has-picture/src/types/user-profile.type.ts
+++ scripts/sync-user-profile-has-picture/src/types/user-profile.type.ts
@@ -1,13 +1,3 @@
 export interface UserProfile {
   UserId: string;
 }
-
-const toUserProfile = (row: UserProfile): UserProfile => {
-  return {
-    UserId: row.UserId,
-  };
-};
-
-export const toUserProfiles = (rows: UserProfile[]): UserProfile[] => {
-  return rows.map((row) => toUserProfile(row));
-};
