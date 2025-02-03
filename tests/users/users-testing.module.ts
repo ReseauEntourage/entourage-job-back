d18d4579d1d12277a6633f9bb6b514334368bcbd
Guillaume Cauchois <guillobits@gmail.com>
2025-02-03 19:47:22 +0100

    fix: tests e2e

--- tests/users/users-testing.module.ts
+++ tests/users/users-testing.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
 import { UserProfilesModule } from 'src/user-profiles/user-profiles.module';
+import { UserSocialSituationsModule } from 'src/user-social-situations/user-social-situations.module';
 import { UsersModule } from 'src/users/users.module';
 import { UserCandidatsHelper } from './user-candidats.helper';
 import { UserProfilesHelper } from './user-profiles.helper';
@@ -8,7 +9,12 @@ import { UserFactory } from './user.factory';
 import { UsersHelper } from './users.helper';
 
 @Module({
-  imports: [UserProfilesModule, UsersModule, AuthModule],
+  imports: [
+    UserProfilesModule,
+    UsersModule,
+    UserSocialSituationsModule,
+    AuthModule,
+  ],
   providers: [
     UsersHelper,
     UserCandidatsHelper,
