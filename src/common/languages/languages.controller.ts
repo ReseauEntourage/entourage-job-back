5038dcfd42b501a3860f6fed053e2fc1809f93e6
Guillaume Cauchois <guillobits@gmail.com>
2025-05-21 15:36:04 +0200

    feat: add route get languages

---
+++ src/common/languages/languages.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Public } from 'src/auth/guards';
+import { LanguagesService } from './languages.service';
+
+@ApiTags('Languages')
+@ApiBearerAuth()
+@Controller('languages')
+export class LanguagesController {
+  constructor(private readonly languagesServices: LanguagesService) {}
+
+  @Public()
+  @Get()
+  async findAll(
+    @Query('limit', new ParseIntPipe()) limit: number,
+    @Query('offset', new ParseIntPipe()) offset: number,
+    @Query('search') search?: string
+  ) {
+    return await this.languagesServices.findAll(limit, offset, search);
+  }
+}
