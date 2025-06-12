7fac424099e30bda8670d37a7d09a6228641f683
Dorian <dorian.pizzato@entourage.social>
2025-06-12 14:42:05 +0200

    Added ghostscript and node version

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,18 @@
 echo "============================"
 echo "= Linkedout API is Running ="
 echo "============================"
+echo "Docker container environment:"
+echo "- Node.js version: $(node -v)"
+echo "- NPM version: $(npm -v)"
+echo "- Operating system: $(uname -a)"
+echo "- Current working directory: $(pwd)"
+echo "- Environment: $NODE_ENV"
+echo "- Port: $PORT"
+echo "=============================""
+
+# Afficher la version de Node.js
+echo "Node.js version:"
+node -v
 
 npm install -g @nestjs/cli
 
