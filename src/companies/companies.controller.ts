ab3e043804758ec9a308014030260c3fbea74645
Guillaume Cauchois <guillobits@gmail.com>
2025-07-23 11:13:38 +0200

    feat(Companies): add inviteCollaborators endpoint and related services for sending invitations

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -2,16 +2,19 @@ import {
   Body,
   Controller,
   Get,
+  Param,
   ParseIntPipe,
   Post,
   Query,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Throttle } from '@nestjs/throttler';
-import { Public } from 'src/auth/guards';
+import { Public, UserPayload } from 'src/auth/guards';
+import { User } from 'src/users/models';
 import { CompaniesService } from './companies.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { CreateCompanyPipe } from './dto/create-company.pipe';
+import { InviteCollaboratorsDto } from './dto/invite-collaborators.dto';
 
 @ApiTags('Companies')
 @Throttle(20, 60)
@@ -39,5 +42,19 @@ export class CompaniesController {
     const createdCompany = await this.companiesService.create(createCompanyDto);
 
     return createdCompany;
+  }
+
+  @Throttle(1, 60)
+  @Post(':companyId/invite-collaborators')
+  async inviteCollaborators(
+    @Param('companyId') companyId: string,
+    @Body() inviteCollaboratorsDto: InviteCollaboratorsDto,
+    @UserPayload() user: User
+  ) {
+    return await this.companiesService.inviteCollaborators(
+      user,
+      companyId,
+      inviteCollaboratorsDto
+    );
   }
 }
--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { MailsService } from 'src/mails/mails.service';
+import { User } from 'src/users/models';
 import { searchInColumnWhereOption } from 'src/utils/misc';
+import { InviteCollaboratorsDto } from './dto/invite-collaborators.dto';
 import { Company } from './models/company.model';
 
 @Injectable()
 export class CompaniesService {
   constructor(
     @InjectModel(Company)
-    private companyModel: typeof Company
+    private companyModel: typeof Company,
+    private readonly mailsService: MailsService
   ) {}
 
   async findAll(limit: number, offset: number, search = '') {
@@ -32,5 +36,35 @@ export class CompaniesService {
       where: { id: companyId },
       order: [['name', 'ASC']],
     });
+  }
+
+  async inviteCollaborators(
+    sender: User,
+    companyId: string,
+    inviteCollaboratorsDto: InviteCollaboratorsDto
+  ) {
+    const company = await this.findOneCompany(companyId);
+    if (!company) {
+      throw new Error(`Company with ID ${companyId} not found`);
+    }
+    if (
+      !inviteCollaboratorsDto.emails ||
+      inviteCollaboratorsDto.emails.length === 0
+    ) {
+      throw new Error('No emails provided for invitation');
+    }
+
+    this.mailsService.sendCompanyInvitationToCollaborators(
+      sender,
+      company,
+      inviteCollaboratorsDto
+    );
+
+    // Logic to handle inviting collaborators, e.g., sending emails
+    // This is a placeholder implementation
+    return {
+      message: 'Invitations sent successfully',
+      emails: inviteCollaboratorsDto.emails,
+    };
   }
 }
