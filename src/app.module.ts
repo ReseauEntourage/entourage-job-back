cae388ebaebba1a4bd4a75aa42d5ea59a06b427e
Guillaume Cauchois <guillobits@gmail.com>
2025-07-25 15:14:44 +0200

    fix: disable db sync

--- src/app.module.ts
+++ src/app.module.ts
@@ -78,7 +78,7 @@ export function getSequelizeOptions(): SequelizeModuleOptions {
     password: password,
     database: pathname.replace('/', ''),
     autoLoadModels: true,
-    synchronize: true,
+    // synchronize: true,
     logging: false,
   };
 }
