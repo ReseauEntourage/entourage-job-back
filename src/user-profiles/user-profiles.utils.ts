3fa5299bce684386f3040d85bea061f5cdfa4fe0
Guillaume Cauchois <guillobits@gmail.com>
2025-08-08 13:20:11 +0200

    feat(UserProfile): search by occupation.name refacto(UserProfile): helps to nudges

--- src/user-profiles/user-profiles.utils.ts
+++ src/user-profiles/user-profiles.utils.ts
@@ -43,5 +43,10 @@ export function userProfileSearchQuery(query = '') {
   return [
     searchInColumnWhereOption('user.firstName', query, true),
     searchInColumnWhereOption('user.lastName', query, true),
+    searchInColumnWhereOption(
+      'sectorOccupations->occupation.name',
+      query,
+      true
+    ),
   ];
 }
