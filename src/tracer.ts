d44773d547cbeb8c9a148b6572aa835aa149afb3
Guillaume Cauchois <guillobits@gmail.com>
2025-11-03 17:43:37 +0100

    debug: blocklist on dd http usage

--- src/tracer.ts
+++ src/tracer.ts
@@ -5,16 +5,14 @@ const ENV = `${process.env.NODE_ENV}`;
 if (ENV === 'production') {
   tracer.init({
     version: process.env.HEROKU_RELEASE_VERSION,
-    plugins: {
-      express: {
-        enabled: true,
-        blacklist: ['/queues/*', '@bull-board/express'],
-      },
-    },
-  } as unknown);
+  });
 
   tracer.use('pg', {
     service: 'linkedout-backend-postgres',
+  });
+
+  tracer.use('http', {
+    blocklist: ['/queues', '/queues/*'],
   });
 }
 
