81485721ff292c315595ebbecab971d7772a0541
Guillaume Cauchois <guillobits@gmail.com>
2025-12-10 18:07:00 +0100

    fix: supprimer la variable WORKER_PORT et son utilisation dans le worker

--- src/worker.ts
+++ src/worker.ts
@@ -3,7 +3,6 @@ import { WorkerModule } from './worker.module';
 
 async function bootstrap() {
   await NestFactory.createApplicationContext(WorkerModule);
-  // await app.listen(process.env.WORKER_PORT || process.env.PORT || 3001);
 }
 
 bootstrap();
