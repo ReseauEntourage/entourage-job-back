12f1446dca31c79f2e0be3265c3778b9f92554d8
Guillaume Cauchois <guillobits@gmail.com>
2026-02-06 14:58:04 +0100

    refactor(CronModule): simplifier les imports en supprimant les modules inutilisés

--- src/cron/cron.module.ts
+++ src/cron/cron.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { SlackModule } from 'src/external-services/slack/slack.module';
 import { QueuesModule } from 'src/queues/producers';
-import { UsersModule } from 'src/users/users.module';
-import { UsersDeletionModule } from 'src/users-deletion/users-deletion.module';
 import { CronService } from './cron.service';
 
 @Module({
-  imports: [UsersModule, UsersDeletionModule, SlackModule, QueuesModule],
+  imports: [QueuesModule],
   providers: [CronService],
 })
 export class CronModule {}
