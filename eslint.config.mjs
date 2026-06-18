533a392dd9bbc143a147c593d20ac1dcdcf5d637
Guillaume Cauchois <guillobits@gmail.com>
2026-06-18 16:48:27 +0200

    chore: mettre à jour la configuration ESLint pour utiliser tsconfig.eslint.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,7 +24,7 @@ export default [
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        project: 'tsconfig.json',
+        project: 'tsconfig.eslint.json',
         sourceType: 'module',
       },
       globals: {
